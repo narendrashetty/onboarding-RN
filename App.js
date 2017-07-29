1768a87f0723d8195a0cdd5e23e4d06513ce749d
Narendra N Shetty <mailnarendra15@gmail.com>
2017-07-29 14:19:08 +0200

    step 2

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { StyleSheet, View, Animated, Dimensions } from 'react-native';
+import { StyleSheet, View, Animated, Dimensions, Easing } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 const size = Math.min(width, height) - 1;
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: new Animated.Value(0)
+    };
+  }
+
+  componentDidMount() {
+    Animated.timing(this.state.scale, {
+      toValue: 4,
+      duration: 1000,
+      easing: Easing.linear
+    }).start();
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -13,7 +29,10 @@ export default class App extends React.Component {
           backgroundColor: '#ccc',
           width: size,
           height: size,
-          borderRadius: size / 2
+          borderRadius: size / 2,
+          transform: [{
+            scale: this.state.scale
+          }]
         }} />
       </View>
     );

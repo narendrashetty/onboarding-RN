79f31eab9ac5648853d417fa6708be9c032e8a5e
Narendra N Shetty <mailnarendra15@gmail.com>
2017-07-29 18:27:14 +0200

    step 9

--- Swipe.js
+++ Swipe.js
@@ -8,13 +8,49 @@ import {
   PanResponder
 } from 'react-native';
 
+const swipeDirections = {
+  SWIPE_LEFT: 'SWIPE_LEFT',
+  SWIPE_RIGHT: 'SWIPE_RIGHT'
+};
+
+function isValidSwipe(velocity, velocityThreshold, directionalOffset, directionalOffsetThreshold) {
+  return Math.abs(velocity) >= velocityThreshold && 
+  Math.abs(directionalOffset) < directionalOffsetThreshold;
+}
+
 export default class Onboarding extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.swipeConfig = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+  }
 
   componentWillMount() {
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: (evt, gestureState) => {
+        const swipeDirection = this._getSwipeDirection(gestureState);
+        console.log(swipeDirection);
+      }
     });
+  }
+
+  _getSwipeDirection(gestureState) {
+    const {SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
+    const {dx, dy} = gestureState;
+    if (this._isValidHorizontalSwipe(gestureState)) {
+      return (dx > 0) ? SWIPE_RIGHT : SWIPE_LEFT;
+    }
+  }
+
+  _isValidHorizontalSwipe(gestureState) {
+    const {vx, dy} = gestureState;
+    const {velocityThreshold, directionalOffsetThreshold} = this.swipeConfig;
+    return isValidSwipe(vx, velocityThreshold, dy, directionalOffsetThreshold);
   }
 
   render() {

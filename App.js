62fe07de9773bb8fad3a366f9d07deebb00cf34d
Narendra N Shetty <mailnarendra15@gmail.com>
2017-07-29 14:22:32 +0200

    step 3

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Animated, Dimensions, Easing } from 'react-native';
+import { 
+  StyleSheet, 
+  View, 
+  Animated, 
+  Dimensions, 
+  Easing, 
+  TouchableWithoutFeedback 
+} from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 const size = Math.min(width, height) - 1;
@@ -13,28 +20,34 @@ export default class App extends React.Component {
     };
   }
 
-  componentDidMount() {
+  onPress() {
     Animated.timing(this.state.scale, {
       toValue: 4,
       duration: 1000,
       easing: Easing.linear
-    }).start();
+    }).start(() => {
+      this.setState({
+        scale: new Animated.Value(0)
+      });
+    });
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Animated.View style={{
-          position: 'absolute',
-          backgroundColor: '#ccc',
-          width: size,
-          height: size,
-          borderRadius: size / 2,
-          transform: [{
-            scale: this.state.scale
-          }]
-        }} />
-      </View>
+      <TouchableWithoutFeedback onPress={this.onPress.bind(this)}>
+        <View style={styles.container}>
+          <Animated.View style={{
+            position: 'absolute',
+            backgroundColor: '#ccc',
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+            transform: [{
+              scale: this.state.scale
+            }]
+          }} />
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }

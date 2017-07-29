92456d4451f410f7c0f78a53bf2f0fb900a6e031
Narendra N Shetty <mailnarendra15@gmail.com>
2017-07-29 15:09:28 +0200

    step 5

--- App.js
+++ App.js
@@ -2,69 +2,23 @@ import React from 'react';
 import { 
   StyleSheet, 
   View, 
-  Animated, 
-  Dimensions, 
-  Easing, 
   TouchableWithoutFeedback 
 } from 'react-native';
-
-const { width, height } = Dimensions.get('window');
-const size = Math.min(width, height) - 1;
+import CircleTransition from './CircleTransition';
 
 export default class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      scale: new Animated.Value(0)
-    };
-  }
-
   onPress() {
-    Animated.timing(this.state.scale, {
-      toValue: 4,
-      duration: 1000,
-      easing: Easing.linear
-    }).start(() => {
-      this.setState({
-        scale: new Animated.Value(0)
-      });
-    });
-  }
-
-  getLeftPosition () {
-    const halfSize = size / 2;
-    const halfWidth = width / 2;
-    let marginHorizontalTopLeft = -halfSize;
-
-    return marginHorizontalTopLeft + halfWidth;
-  }
-
-  getTopPosition () {
-    const halfSize = size / 2;
-    let marginVerticalTopLeft = -halfSize;
-
-    return marginVerticalTopLeft + height;
+    this.circleTransition.animate();
   }
 
   render() {
-    let topPosition = this.getTopPosition();
-    let leftPosition = this.getLeftPosition();
     return (
       <TouchableWithoutFeedback onPress={this.onPress.bind(this)}>
         <View style={styles.container}>
-          <Animated.View style={{
-            position: 'absolute',
-            backgroundColor: '#ccc',
-            width: size,
-            height: size,
-            left: leftPosition,
-            top: topPosition,
-            borderRadius: size / 2,
-            transform: [{
-              scale: this.state.scale
-            }]
-          }} />
+          <CircleTransition
+            ref={(circle) => { this.circleTransition = circle }}
+          />
         </View>
       </TouchableWithoutFeedback>
     );
---
+++ CircleTransition.js
@@ -0,0 +1,77 @@
+import React, { Component, PropTypes } from 'react';
+import { Easing, Modal, Dimensions, Animated } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+class CircleTransition extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      scale: new Animated.Value(0),
+      color: '#ccc'
+    };
+  }
+
+  animate() {
+    Animated.timing(this.state.scale, {
+      toValue: 4,
+      duration: this.props.duration,
+      easing: this.props.easing
+    }).start(() => {
+      this.setState({
+        scale: new Animated.Value(0)
+      });
+    });
+  }
+
+  getLeftPosition () {
+    const halfSize = this.props.size / 2;
+    const halfWidth = width / 2;
+    let marginHorizontalTopLeft = -halfSize;
+
+    return marginHorizontalTopLeft + halfWidth;
+  }
+
+  getTopPosition () {
+    const halfSize = this.props.size / 2;
+    let marginVerticalTopLeft = -halfSize;
+
+    return marginVerticalTopLeft + height;
+  }
+
+  render () {
+    const {scale, color} = this.state;
+    const { size } = this.props;
+    let topPosition = this.getTopPosition();
+    let leftPosition = this.getLeftPosition();
+    return (
+      <Animated.View style={{
+        position: 'absolute',
+        backgroundColor: color,
+        top: topPosition,
+        left: leftPosition,
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        transform: [{
+          scale: scale
+        }]
+      }} />
+    )
+  }
+}
+
+CircleTransition.propTypes = {
+  size: PropTypes.number,
+  duration: PropTypes.number,
+  easing: PropTypes.func,
+}
+
+CircleTransition.defaultProps = {
+  size: Math.min(width, height) - 1,
+  duration: 400,
+  easing: Easing.linear
+}
+
+export default CircleTransition;
